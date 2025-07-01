60c8f1a550b405749300268065b2c0dbba713a76
Raakeshgiri <raakesh910@gmail.com>
2025-07-01 21:43:26 +0530

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+let name = "Raakesh";
+let age = 22;
+
+let App = ()=>
+{
+  return (
+    <div>
+
+      <h1>Hello everyone,Welcome to React Class</h1>
+      {/* <p>Welcome to React Class.</p> */}
+      {/* JSX -> JavaScript XML */}
+
+      <h2>Name: {name}</h2>
+      <h3>My age: {age}</h3>
+
+      <hr />
+    </div>
+  )
+}
+
+export default App
